f29b6b509a1e391d2928ce6840de8e603f78792a
Paul Mannarino <paul.mannarino@gmail.com>
2020-05-09 15:43:49 -0400

    Substitued so many variables for dicts

--- specdumbot.py
+++ specdumbot.py
@@ -14,57 +14,69 @@ def sendTweet(db_client, tw_client):
   )
   daily_points = list(results_set.get_points())
 
-  # TODO: improve average calculation
   # init average data values
-  download_count, download_sum , dowload_dip_count = 0, 0, 0
-  upload_count, upload_sum = 0, 0
-  ping_count, ping_sum = 0, 0
-  download_low, upload_low, ping_high = float("inf"), float("inf"), 0
+  download = {
+    'low': float("inf"),
+    'count': 0,
+    'sum': 0,
+    'dip_count': 0,
+    'avg': 0
+  }
+  upload = {
+    'low': float("inf"),
+    'count': 0,
+    'sum': 0,
+    'avg': 0
+  }
+  ping = {
+    'high': 0,
+    'count': 0,
+    'sum': 0,
+    'avg': 0
+  }
 
+  # TODO: there's gotta be a better way to iterate through the points
   for point in daily_points:
     if point['download']: # skip empty datapoints
-      download_count += 1
-      download_sum += point['download']
+      download['count'] += 1
+      download['sum'] += point['download']
       if point['download'] < 200:
-        dowload_dip_count += 1
-      if point['download'] < download_low:
-        download_low = point['download']
+        download['dip_count'] += 1
+      if point['download'] < download['low']:
+        download['low'] = round(point['download'], 1)
     if point['upload']:
-      upload_count += 1
-      upload_sum += point['upload']
-      if point['upload'] < upload_low:
-        upload_low = point['upload']
+      upload['count'] += 1
+      upload['sum'] += point['upload']
+      if point['upload'] < upload['low']:
+        upload['low'] = round(point['upload'], 1)
     if point['ping']:
-      ping_count += 1
-      ping_sum += point['ping']
-      if point['ping'] > ping_high:
-        ping_high = point['ping']
+      ping['count'] += 1
+      ping['sum'] += point['ping']
+      if point['ping'] > ping['high']:
+        ping['high'] = round(point['ping'], 1)
 
-  download_avg = round(download_sum / download_count, 1)
-  upload_avg = round(upload_sum / upload_count, 1)
-  ping_avg = round(ping_sum / ping_count, 1)
-
-  download_low = round(download_low, 1)
-  upload_low = round(upload_low, 1)
-  ping_high =  round(ping_high, 1)
+  # TODO: move logic to function? Calculate moving average while iterating data above?
+  download['avg'] = round(download['sum'] / download['count'], 1)
+  upload['avg'] = round(upload['sum'] / upload['count'], 1)
+  ping['avg'] = round(ping['sum'] / ping['count'], 1)
 
   measurements_date = date.today().strftime("%B %d, %Y") # May 04, 2020
 
   tweet_text = f"""\
   Internet Speed metrics for {measurements_date} (Queens, NY):
 
-  Download (Mbit/s): Average {download_avg}, Low {download_low}
-  Upload (Mbit/s): Average {upload_avg}, Low {upload_low}
-  Ping (ms): Average {ping_avg}, High {ping_high}
+  Download (Mbit/s): Average {download['avg']}, Low {download['low']}
+  Upload (Mbit/s): Average {upload['avg']}, Low {upload['low']}
+  Ping (ms): Average {ping['avg']}, High {ping['high']}
 
-  Download speeds below 200 Mbit/s {dowload_dip_count} times
+  Download speeds below 200 Mbit/s {download['dip_count']} times
 
   @GetSpectrum
   #spectrum #specdumb #internetSpeed #speedTest\
   """
 
   tweet_text = dedent(tweet_text)
-  
+  print(tweet_text)
   # update twitter status
-  tw_client.update_status(status=tweet_text)
+  # tw_client.update_status(status=tweet_text)
 
